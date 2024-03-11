dfd8f658e88679873eb4a73e04e49913eff09dc0
mqnx <93342638+mqnx@users.noreply.github.com>
2024-03-11 15:54:03 +0100

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
 npm install discord.js
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('message', message => {
+  if (message.content === '!hello') {
+    message.channel.send('Hello, world!');
+  }
+});
+
+client.login('your-bot-token-goes-here');
